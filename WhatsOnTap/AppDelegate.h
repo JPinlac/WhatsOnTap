24b8c702a935b80f29d2f6d6cb952fd896666377
Jonathan Pinlac <jonppinlac@gmail.com>
2016-06-13 15:41:56 -0400

    Initial commit

---
+++ WhatsOnTap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhatsOnTap
+//
+//  Created by DetroitLabs on 6/13/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhatsOnTap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatsOnTap
+//
+//  Created by DetroitLabs on 6/13/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
