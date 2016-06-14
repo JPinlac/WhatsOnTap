a86db33954636d73a3c6f14aaa80092e08413c87
Jonathan Pinlac <jonppinlac@gmail.com>
2016-06-14 09:55:34 -0400

    Stash applygits! and simple organizing

---
+++ WhatsOnTap/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  WhatsOnTap
+//
+//  Created by DetroitLabs on 6/14/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ WhatsOnTap/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  WhatsOnTap
+//
+//  Created by DetroitLabs on 6/14/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
