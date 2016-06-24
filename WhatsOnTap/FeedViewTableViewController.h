69abd57a0d0c7ebb2c920e6d94a100273586f569
Brandon Manson <brandon.manson@detroitlabs.com>
2016-06-23 20:00:57 -0400

    added FeedTableViewController class

---
+++ WhatsOnTap/FeedViewTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FeedViewTableViewController.h
+//  WhatsOnTap
+//
+//  Created by Brandon Manson on 6/23/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedViewTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *updates;
+
+@end
